0f9ab796eb841c906e6ae6d268c01de5cb814c3b
Maksim Korelov <korelov@gmail.com>
2022-09-10 13:49:49 +0300

    Work on project. Stage 2/4: Convert to decimal

--- Number Base Converter/task/src/converter/Main.java
+++ Number Base Converter/task/src/converter/Main.java
@@ -3,7 +3,8 @@ package converter;
 import java.util.Scanner;
 
 public class Main {
-    public static String converter(int number, int base) {
+
+    public static String fromDecimal(int number, int base) {
         String hex = "0123456789ABCDEF";
         StringBuilder stringBuilder = new StringBuilder();
         while (number > 0) {
@@ -13,13 +14,73 @@ public class Main {
         return stringBuilder.reverse().toString();
     }
 
+    public static String fromBinaryOcta(int number, int base) {
+        int result = 0;
+        int n = 0;
+        while (number > 0) {
+            result += number % 10 * Math.pow(base, n);
+            number = number / 10;
+            n++;
+        }
+        return Integer.toString(result);
+    }
+
+    public static String fromHex(String number, int base) {
+        String digits = "0123456789ABCDEF";
+        number = number.toUpperCase();
+        int result = 0;
+        for (int i = 0; i < number.length(); i++) {
+            char c = number.charAt(i);
+            int d = digits.indexOf(c);
+            result = base * result + d;
+        }
+        return Integer.toString(result);
+    }
+
+    public static void fromDecimal() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number in decimal system: ");
+        int number = scanner.nextInt();
+        System.out.print("Enter target base: ");
+        int base = scanner.nextInt();
+        System.out.println("Conversion result: " + fromDecimal(number, base));
+    }
+
+    public static void toDecimal() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter source number: ");
+        String number = scanner.nextLine();
+        System.out.print("Enter source base: ");
+        int base = scanner.nextInt();
+        switch (base) {
+            case 2:
+            case 8:
+                System.out.println("Conversion to decimal result: " + fromBinaryOcta(Integer.parseInt(number), base));
+                break;
+            case 16:
+                System.out.println("Conversion to decimal result: " + fromHex(number, base));
+                break;
+        }
+    }
+
+    public static void choseConvert() {
+        Scanner scanner = new Scanner(System.in);
+        String chose = "";
+        do {
+            System.out.print("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ");
+            chose = scanner.nextLine();
+            switch (chose) {
+                case "/from":
+                    fromDecimal();
+                    break;
+                case "/to":
+                    toDecimal();
+                    break;
+            }
+        } while (!chose.equals("/exit"));
+    }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number in decimal system:");
-        int number = scanner.nextInt();
-        System.out.print("Enter target base:");
-        int base = scanner.nextInt();
-        System.out.println("Conversion result: " + converter(number, base));
+        choseConvert();
     }
 }
