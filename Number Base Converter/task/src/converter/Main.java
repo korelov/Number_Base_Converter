779027da4a0608d8d7b31eba9b50e20b8c58e53a
Maksim Korelov <korelov@gmail.com>
2022-09-10 21:36:12 +0300

    Work on project. Stage 3/4: Convert to any base

--- Number Base Converter/task/src/converter/Main.java
+++ Number Base Converter/task/src/converter/Main.java
@@ -1,11 +1,13 @@
 package converter;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
 
+
     public static String fromDecimal(int number, int base) {
-        String hex = "0123456789ABCDEF";
+        String hex = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         StringBuilder stringBuilder = new StringBuilder();
         while (number > 0) {
             stringBuilder.append(hex.charAt(number % base));
@@ -26,7 +28,7 @@ public class Main {
     }
 
     public static String fromHex(String number, int base) {
-        String digits = "0123456789ABCDEF";
+        String digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         number = number.toUpperCase();
         int result = 0;
         for (int i = 0; i < number.length(); i++) {
@@ -57,27 +59,57 @@ public class Main {
             case 8:
                 System.out.println("Conversion to decimal result: " + fromBinaryOcta(Integer.parseInt(number), base));
                 break;
-            case 16:
+            case 36:
                 System.out.println("Conversion to decimal result: " + fromHex(number, base));
                 break;
         }
     }
 
+//    public static void choseConvert() {
+//        Scanner scanner = new Scanner(System.in);
+//        String chose = "";
+//        do {
+//            System.out.print("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ");
+//            chose = scanner.nextLine();
+//            switch (chose) {
+//                case "/from":
+//                    fromDecimal();
+//                    break;
+//                case "/to":
+//                    toDecimal();
+//                    break;
+//            }
+//        } while (!chose.equals("/exit"));
+//    }
+
     public static void choseConvert() {
         Scanner scanner = new Scanner(System.in);
-        String chose = "";
+        System.out.print("Enter two numbers in format: {source base} {target base} (To quit type /exit) ");
+        String sourceBase1 = scanner.next();
+        if(sourceBase1.equals("/exit")){
+            System.exit(0);
+        }
+
+        int sourceBase=Integer.parseInt(sourceBase1);
+        int targetBase = scanner.nextInt();
+        scanner.nextLine();
+
+        String number = "";
+
         do {
-            System.out.print("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ");
-            chose = scanner.nextLine();
-            switch (chose) {
-                case "/from":
-                    fromDecimal();
-                    break;
-                case "/to":
-                    toDecimal();
-                    break;
+            System.out.printf("Enter number in base %d to convert to base %d (To go back type /back) ", sourceBase, targetBase);
+            number = scanner.nextLine();
+            if (number.equals("/back")) {
+                choseConvert();
             }
-        } while (!chose.equals("/exit"));
+            if (sourceBase == 10) {
+                BigInteger bigInteger = new BigInteger(number);
+                System.out.println("Conversion result: " + bigInteger.toString(targetBase));
+            } else {
+                BigInteger bigInteger = new BigInteger(number, sourceBase);
+                System.out.println("Conversion result: " + bigInteger.toString(targetBase));
+            }
+        } while (!number.equals("/exit"));
     }
 
     public static void main(String[] args) {
